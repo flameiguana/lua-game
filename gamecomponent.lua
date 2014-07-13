ccc4ad7cc12af1b18facc88904c6a34f5b752734
flameiguana <flameiguana@gmail.com>
2014-07-12 23:55:27 -0700

    separated gameobject and gamecomponent source files. added draw methods

--- gamecomponent.lua
+++ gamecomponent.lua
@@ -15,47 +15,16 @@ function GameComponent:update()
 
 end
 
+function GameComponent:draw()
+
+end
+
 function GameComponent:disable()
 
 end
 --by default components don't update, unless the subclass says to do so
 GameComponent.static.canUpdate = false
-
-
---The game object class supports adding components and retrieving them
-GameObject = class('GameObject')
-
-function GameObject:initialize()
-	self.updatingComponents = nil
-end
-
-function GameObject:addComponent(Type)
-	local instanceOfType = Type:new()
-
-	instanceOfType.gameObject = self
-
-	--add publicly accesible variable. TODO: make fist letter lowercase
-	--you can access like this: gameObject.Type (eg. player.boundingBox)
-	self[Type.name] = instanceOfType
-
-	--if this is a component that updates, add to list
-	if Type.canUpdate then
-		self.updatingComponents = {next = self.updatingComponents, value = instanceOfType}
-	end
-
-	return instanceOfType
-end
-
---updates all attached components
-function GameObject:update()
-	--create a local pointer to traverse list
-	local updatingComponents = self.updatingComponents
-	-- traverse list
-	while updatingComponents do
-		updatingComponents.value:update()
-		updatingComponents = updatingComponents.next
-	end
-end
+GameComponent.static.canDraw = false
 
 
 --a test component that can be added to an object
---
+++ gameobject.lua
@@ -0,0 +1,51 @@
+--The game object class supports adding components and retrieving them
+GameObject = class('GameObject')
+
+function GameObject:initialize()
+	self.updatingComponents = nil
+end
+
+--TODO, maybe let user initialize object instead of callng default constructor (would imply player is owner though)
+function GameObject:addComponent(Type)
+	local instanceOfType = Type:new()
+
+	instanceOfType.gameObject = self
+
+	--add publicly accesible variable. TODO: make fist letter lowercase
+	--you can access like this: gameObject.Type (eg. player.boundingBox)
+	
+	self[Type.name] = instanceOfType
+
+	--if this is a component that updates, add to list
+	if Type.canUpdate then
+		self.updatingComponents = {next = self.updatingComponents, value = instanceOfType}
+	end
+
+	if Type.canDraw then
+		self.drawingComponents = {next = self.drawingComponents, value = instanceOfType}
+	end
+
+	return instanceOfType
+end
+
+--updates all attached components
+function GameObject:update()
+	--create a local pointer to traverse list
+	local updatingComponents = self.updatingComponents
+	-- traverse list
+	while updatingComponents do
+		updatingComponents.value:update()
+		updatingComponents = updatingComponents.next
+	end
+
+end
+
+function GameObject:draw()
+	local drawingComponents = self.drawingComponents
+	-- traverse list
+	while drawingComponents do
+		drawingComponents.value:draw()
+		drawingComponents = drawingComponents.next
+	end
+end
+
